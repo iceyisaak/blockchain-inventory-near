5e77753637d9dc5295f3831b7635a7a6305b6b3f
iceyisaak <me@iceyisaak.com>
2021-12-30 12:39:20 +0100

    Updated repo structure + minor changes to smartcontract

--- src/inventory/assembly/index.ts
+++ src/inventory/assembly/index.ts
@@ -1,4 +1,4 @@
-import { storage, Context, logging } from "near-sdk-core"
+import { Context, logging } from "near-sdk-core"
 import { AccountId } from "../../utils"
 import { ProductDetail, Products } from "./model"
 
@@ -27,7 +27,6 @@ export class Contract {
     this.assert_owner()
     assert(title.length > 0, "Title is required.")
     assert(price >= 0, "Price is required.")
-
     Products.push(new ProductDetail(Products.length, title, description, price, Context.blockTimestamp))
     return true
   }
@@ -46,37 +45,10 @@ export class Contract {
     return Products[id]
   }
 
-
-  // private helper method used by read() and write() above
-  private storageReport(): string {
-    return `storage [ ${Context.storageUsage} bytes ]`
-  }
-
   private assert_owner(): void {
     const caller = Context.predecessor
-    assert(this.owner == caller, "Only the owner of this contract may call this method")
+    assert(this.owner == caller, "You have no right to call such method ;)")
   }
 
-}
-
-/**
- * This function exists only to avoid a compiler error
- *
-
-ERROR TS2339: Property 'contains' does not exist on type 'src/singleton/assembly/index/Contract'.
-
-     return this.contains(key);
-                 ~~~~~~~~
- in ~lib/near-sdk-core/storage.ts(119,17)
-
-/Users/sherif/Documents/code/near/_projects/edu.t3/starter--near-sdk-as/node_modules/asbuild/dist/main.js:6
-        throw err;
-        ^
-
- * @param key string key in account storage
- * @returns boolean indicating whether key exists
- */
-function isKeyInStorage(key: string): bool {
-  return storage.hasKey(key)
 }
 
